f72104701a730dd5b36967383bdf839a9a567030
Konrad Karas <konradkar2@gmail.com>
2023-10-07 13:30:14 +0200

    Run docker on unit_tests/memcheck

--- buildnrun.sh
+++ buildnrun.sh
@@ -6,7 +6,7 @@ name=ripv2_container
 docker kill $name
 docker rm $name
 
-
+set -e
 docker build -t ripv2 -f docker/Dockerfile .
 docker run -it --privileged -e DISPLAY -v /lib/modules:/lib/modules --name $name -d ripv2
            
--- src/tests/main.c
+++ src/tests/main.c
@@ -5,6 +5,6 @@ int main(int argc, char **argv)
 {
 	(void)argc;
 	(void)argv;
-
+	
 	return run_all_tests();
 }
